443fa93a6985eda95746c5ac65d9bb6b65f2f065
DK <dasha.krutsko@gmail.com>
2022-12-13 11:21:13 +0300

    update

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
+
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> swapMap = new HashMap<>();
+        for (Map.Entry<Integer, String> e : sourceMap.entrySet()) {
+            if (swapMap.containsKey(e.getValue())) {
+                if (swapMap.get(e.getValue()) < e.getKey()) {
+                    swapMap.put(e.getValue(), e.getKey());
+                }
+            } else {
+                swapMap.put(e.getValue(), e.getKey());
+            }
+        }
+        return swapMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,22 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        if (sentence != null && sentence.trim().length() != 0) {
+            String[] words = sentence.trim().replace(".", "").replace(",", "").toLowerCase(Locale.ROOT).split(" ");
+            for (String word : words) {
+                if (hashMap.containsKey(word)) {
+                    hashMap.put(word, hashMap.get(word) + 1);
+                } else {
+                    hashMap.put(word, 1);
+                }
+            }
+        }
+        return hashMap;
     }
 }
